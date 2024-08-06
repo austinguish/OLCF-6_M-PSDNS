be4241a810857f133ea3506048ab4eaa5466d408
Jiang Yiwei <479772069@qq.com>
2024-08-06 13:19:40 -0500

    chore: Update build script for lonestar environment

--- scripts_lonestar/build_dns_lonestar.sh
+++ scripts_lonestar/build_dns_lonestar.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-source setUpModules_summit.sh
-
-module list
-
 if [ ! -d ".srcmake" ]; then
-   make -f makefile.summit srcmake
+   make -f makefile.lonestar srcmake
 fi
 
-make -f makefile.summit clean
-make -f makefile.summit
+make -f makefile.lonestar clean
+make -f makefile.lonestar
 
 # check the executable in this environment
-ldd DNS_PEN_GPU_p4.x
+ldd DNS_PEN_GPU_p*.x
